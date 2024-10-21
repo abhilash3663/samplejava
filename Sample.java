69ed17b721a418115b5455ce8877f9aefec3bec4
abhilash <abhilashvodela03@gmail.com>
2024-10-21 22:47:29 +0530

    sample java program

---
+++ Sample.java
@@ -0,0 +1,7 @@
+public class Sample {
+    // Main method: the entry point of the application
+    public static void main(String[] args) {
+        // Print "Hello World" to the console
+        System.out.println("Hello World");
+    }
+}
