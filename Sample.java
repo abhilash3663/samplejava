d3dcb79b889bdad2fe895fadca3c0e3fa0def143
anirudhpinreddy <anirudhreddypinreddy@gmail.com>
2024-10-22 14:27:32 +0530

    SRIRAM

--- Sample.java
+++ Sample.java
@@ -3,5 +3,6 @@ public class Sample {
     public static void main(String[] args) {
         // Print "Hello World" to the console
         System.out.println("Hello World");
+	System.out.println(" Hello im Sriram");
     }
 }
